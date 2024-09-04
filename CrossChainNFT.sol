40aaaecbed78ff0fa487f6d85aced32f6c1d3169
Manan Garg <92792271+Coder-Manan@users.noreply.github.com>
2024-09-04 22:22:31 +0530

    Create CrossChainNFT.sol

---
+++ CrossChainNFT.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { ONFT721 } from "@layerzerolabs/onft-evm/contracts/onft721/ONFT721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CrossChainNFT is ONFT721 {
+
+    uint256 public tokenCounter;
+    address private _chatBotContractAddress;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _lzEndpoint,
+        address _delegate
+    ) ONFT721(_name, _symbol, _lzEndpoint, _delegate) {
+        tokenCounter = 0;
+    }
+
+    function setChatBotContractAddress(address contractAddress) external onlyOwner {
+        _chatBotContractAddress = contractAddress;
+    }
+
+    function approvalRequired() external pure override returns (bool) {
+        return false;
+    }
+
+    // so that only the chat bot contract can ask to mint NFTs
+    modifier onlyChatBotContract {
+        require(msg.sender == _chatBotContractAddress);
+        _;
+    }
+
+    function createNFT(string memory tokenURI, address to) public onlyChatBotContract {
+        // TODO: LayerZero currently, use ChainLink CCIP if LayerZero cant support
+    }
+}
